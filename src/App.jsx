7c668b0c4f0b0b7a7cfeb6a73c4224ddeaffea38
Pramod shivaji thete <136362959+Pramodthete@users.noreply.github.com>
2024-01-29 12:36:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <button className='btn btn-primary'>primary</button>
+      <button className='btn btn-secondary'>secondary</button>
+      <button className='btn btn-success'>success</button>
+      <button className='btn btn-danger'>danger</button>
+      <button className='btn btn-info'>info</button>
+      <button className='btn btn-dark'>dark</button>
+      <button className='btn btn-light'>light</button>
+      <button className='btn btn-link'>Link</button>
+    </>
+  )
+}
+
+export default App
